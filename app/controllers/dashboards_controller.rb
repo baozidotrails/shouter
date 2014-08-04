ba5ded02d0229f243659454410a1e3c63bac23c8
Baozi <baozi.rails@gmail.com>
2014-08-04 19:43:38 +0800

    Add bootstrap-sass and shout basic

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def show
-    
+    @shout = Shout.new
+    @shouts = Shout.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
   
 
   root 'pages#home', via: :get
 
   resource :dashboard, only: [:show]
+  resources :shouts, only: [:create]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 end
